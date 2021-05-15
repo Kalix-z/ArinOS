f8354a4cbdd772f0e225e549924155a68c6ceea1
Kalix-z <84145117+Kalix-z@users.noreply.github.com>
2021-05-15 12:31:45 -0700

    Update Bootloader.asm

--- Bootloader.asm
+++ Bootloader.asm
@@ -1,50 +1,101 @@
 [bits 16]
-[org 0x7c00]
 
-mov ah, 0x00 ;bios interupt for clearing screen
-mov al, 0x00
-int 0x10
+
+
+;[org 0x7c00]
+
+
+
 jmp Enable32BitMode
+
 %include "gdt.asm"
 
 
 
-global start
 
-;[extern _start]
 
-;call _start
-section .text
 Enable32BitMode:
+
+    cli      ;disables interupts
+
     call enable_A20
-    cli ;disables interupts
+
     lgdt [gdt_descriptor]
+
     mov eax, cr0 
+
     or eax, 1       ; set PE (Protection Enable) bit in CR0 (Control Register 0)
+
     mov cr0, eax
+
     jmp codeseg:Start32Bit
 
+
+
 enable_A20:
+
     in al, 0x92
+
     or al, 2
+
     out 0x92, al
+
     ret       
 
+
+
 [bits 32]
+
+[extern _start]
+
+
+
 Start32Bit:
+
+
+
+
+
     mov ax, dataseg ;moves dataseg (found in gdt, into registers ds to gs)
+
     mov ds, ax
+
     mov ss, ax
+
     mov es, ax
+
     mov fs, ax
+
     mov gs, ax
 
-    mov ebp, 0x90000 ;clears stack
-    mov esp, ebp    
-    %include "StartingText.asm"
-    jmp $
+    call _start
 
-  
-times 510 - ($ - $$) db 0 ;fills the rest of the bytes till byte 510 with 0s
+mov edx, 0xb8000 ;clears the screen by looping through all the pixels and adding a space character (0x20) with the color black (0x00) 
+
+mov cx, 5000 ;times to loop 
+
+    ClearScreen:        
+
+        mov [edx], byte 0x20 ;Square brackets due to wanting to move the value specified (0x20) into the address in edx, not into the register
+
+        add edx, 2
+
+        
+
+
+
+        ;call _start ;_start();
+
+loop ClearScreen
+
+jmp $
+
+
+
+times 510-($-$$) db 0 ;fills the rest of the bytes till byte 510 with 0s
+
 db 0x55 ;sets the last 2 bytes to 0x55 and 0xaa, so the bios knows its bootable
+
 db 0xaa
+
+
