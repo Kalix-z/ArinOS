6589eb547324eb7543245f11500e06a4e1f57371
Kalix-z <84145117+Kalix-z@users.noreply.github.com>
2021-05-15 14:04:43 -0700

    Update Bootloader.asm

--- Bootloader.asm
+++ Bootloader.asm
@@ -10,7 +10,35 @@ jmp Enable32BitMode
 
 %include "gdt.asm"
 
+mov ah, 0x02 ; ah <- 'read' function
 
+mov al, 0x01   ; al <- sectors to read (only 512 bytes)
+
+mov cl, 0x02 ; cl 
+
+             ; 1 is  boot sector, 2 is the first 'available' sector
+
+mov ch, 0x00 ; ch <- cylinder 
+
+mov dh, 0x00 ; dh <- head number 
+
+mov dl, 0x00
+
+; 00H-7FH floppy disk
+
+; 80H-FFH fixed disk
+
+
+
+; dl <- drive number
+
+; (0 = floppy, 1 = floppy2, 0x80 = hdd, 0x81 = hdd2)
+
+
+
+mov bx, 0x9000  ; location where we load our kernel code
+
+int 0x13  
 
 
 
@@ -44,7 +72,17 @@ enable_A20:
 
 
 
+
+
+
+
+jmp word 0x0008:0x9000
+
+
+
 [bits 32]
+
+
 
 [extern _start]
 
@@ -70,25 +108,9 @@ Start32Bit:
 
     call _start
 
-mov edx, 0xb8000 ;clears the screen by looping through all the pixels and adding a space character (0x20) with the color black (0x00) 
-
-mov cx, 5000 ;times to loop 
-
-    ClearScreen:        
-
-        mov [edx], byte 0x20 ;Square brackets due to wanting to move the value specified (0x20) into the address in edx, not into the register
-
-        add edx, 2
-
-        
 
 
 
-        ;call _start ;_start();
-
-loop ClearScreen
-
-jmp $
 
 
 
@@ -97,5 +119,10 @@ times 510-($-$$) db 0 ;fills the rest of the bytes till byte 510 with 0s
 db 0x55 ;sets the last 2 bytes to 0x55 and 0xaa, so the bios knows its bootable
 
 db 0xaa
+
+
+
+
+
 
 
